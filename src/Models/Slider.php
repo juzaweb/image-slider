9a322cc408882d476c3f1c3c96911e1322b9624f
Juzaweb <dangtheanh16@gmail.com>
2021-09-20 22:15:52 +0700

    :construction: Fix load theme

--- src/Models/Slider.php
+++ src/Models/Slider.php
@@ -5,6 +5,15 @@ namespace Juzaweb\ImageSlider\Models;
 use Illuminate\Database\Eloquent\Model;
 use Juzaweb\Traits\ResourceModel;
 
+/**
+ * Juzaweb\ImageSlider\Models\Slider
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Slider newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Slider newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Slider query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Slider whereFilter($params = [])
+ * @mixin \Eloquent
+ */
 class Slider extends Model
 {
     use ResourceModel;
